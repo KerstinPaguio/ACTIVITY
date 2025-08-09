67bb40de6553d97995a34f6fb6c5d3728767b797
Kerstin Paguio <kspaguio1@student.hau.edu.ph>
2025-08-09 11:23:11 +0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,12 @@
+# tweets/models.py
+from django.db import models
+
+class Tweet(models.Model):
+    content = models.CharField(max_length=280)
+    image = models.ImageField(upload_to='tweets/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        # first 30 chars, just for admin readability
+        return self.content[:30]
+
